bc9903c81c5050cdd83e09dcd9d7d91d7ab9195f
VIMbjar <arianbrjbiziness@gmail.com>
2025-03-17 23:25:40 +0330

    ep3 added

---
+++ ep3.py
@@ -0,0 +1,37 @@
+# largest prime factor of number : 600851475143
+number = 600851475143
+
+
+def isprime(x):
+    if x <= 1:
+        return False
+    elif x == 2:
+        return True
+    elif x != 2 and x % 2 == 0 :
+        return False
+    else:
+        flag = True
+
+        for i in range(3,int(x**.5),2):
+            if x % i == 0:
+                flag = False
+                break
+            # x = q//i
+        return flag
+    
+def factors(n):
+    factors = []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            factors.append(i)
+            if i * i != n:  # Avoid adding square root twice for perfect squares
+                factors.append(n // i)
+        i += 1
+    return sorted(factors)
+
+
+big_number_factors = [j for j in factors(number) if isprime(j)]
+
+# [71, 839, 1471, 6857]
+print(max(big_number_factors))
